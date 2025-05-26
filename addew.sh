3854b20309afd5ebe0f67dadee735a616b75567c
Anatolii Charikov <acharikov@proton.me>
2025-05-27 01:04:28 +0300

    Update script. Change directory structure.
    Signed-off-by: Anatolii Charikov <acharikov@proton.me>

--- addew.sh
+++ addew.sh
@@ -5,87 +5,96 @@ if [ "$1" = "" ]; then
 	exit 1;
 fi
 
-# directories definitions:
-PROJECT_DIR=$1	
+PROJECT_DIR=$1
 PROJECT_NAME=$1
-BUILD_DIR=build
-INCLUDE_DIR=include
+PROJ_MAIN_BIN=$1
 SRC_DIR=src
 
-SRC_FILE_EXTENSION=.c
-
-# generating directories:
+#### generating directories: ####
 mkdir -p	$PROJECT_DIR \
-			$PROJECT_DIR/$BUILD_DIR \
-			$PROJECT_DIR/$INCLUDE_DIR \
-			$PROJECT_DIR/$SRC_DIR
+		$PROJECT_DIR/$SRC_DIR
+# ---------------------------------- #
 
-# generating the "main" file:
-touch $PROJECT_DIR/$SRC_DIR/$PROJECT_NAME$SRC_FILE_EXTENSION
+cd $PROJECT_DIR
 
-# generating the Makefile:
-touch $PROJECT_DIR/Makefile
+#### general Makefile generating: ####
+cat > Makefile << EOF
+#
 
-cat > $PROJECT_DIR/Makefile << EOF
-CC=gcc -ansi -fdiagnostics-color=always -fno-diagnostics-show-caret
-CFLAGS=-Wall -ggdb -I
-LIBS=
+default:
+	@cd src && \$(MAKE) --no-print-directory
 
-SRC_FILE_EXTENSION = .c
-HEADER_FILE_EXTERNSION = .h
+all:	default
 
-# directories definitions:
-BUILD_DIR	=	build
-INCLUDE_DIR	=	include
-SRC_DIR		=	src
-
-# modules enumeration:
-# SRC_FILES	=	\$(SRC_DIR)/\$(SRC_FILE_EXTENSION)
-SRC_FILES = \$(filter-out \$(SRC_DIR)/$PROJECT_NAME$SRC_FILE_EXTENSION, \$(wildcard \$(SRC_DIR)/*$SRC_FILE_EXTENSION))
-
-# set object files to the build directory:
-OBJECT_FILES=\
-	\$(subst \$(SRC_DIR),\
-	\$(BUILD_DIR),\
-	\$(SRC_FILES:\
-	\$(SRC_FILE_EXTENSION)=.o))
-
-HEADER_FILES=	\$(wildcard \$(INCLUDE_DIR)/*\$(HEADER_FILE_EXTERNSION)
-
-\$(BUILD_DIR)/%.o: \$(SRC_DIR)/%\$(SRC_FILE_EXTENSION) \$(INCLUDE_DIR)/%\$(HEADER_FILE_EXTERNSION)
-	@\$(CC) \$(CFLAGS)\$(INCLUDE_DIR) -c \$< -o \$@
-
-# targets:
-$PROJECT_NAME: \$(SRC_DIR)/$PROJECT_NAME\$(SRC_FILE_EXTENSION) \$(OBJECT_FILES) -l\$(LIBS)
-	@\$(CC) \$(CFLAGS) \$(INCLUDE_DIR) \$^ -o \$(BUILD_DIR)/\$@
-
-.PHONY: run
-run: $PROJECT_NAME
-	@./\$(BUILD_DIR)/$PROJECT_NAME
-
-.PHONY: clean
 clean:
-	@rm -f	\$(BUILD_DIR)/*.o \$(BUILD_DIR)/$PROJECT_NAME
+	@cd src && \$(MAKE) clean --no-print-directory
 
-.PHONY: tags
-tags:
-	@ctags \$(INCLUDE_DIR)/*\$(SRC_FILE_EXTENSION) \$(SRC_DIR)/*\$(SRC_FILE_EXTENSION)
+run:	all
+	@./src/$PROJ_MAIN_BIN
+
+.PHONY: default all clean run
 EOF
+# ---------------------------------- #
 
-# generating the .gitignore file:
-	touch $PROJECT_DIR/.gitignore
-	cat > $PROJECT_DIR/.gitignore << EOF
-# The gitignore file specifies intentionally untracked files that Git should ignore.
-#
-EOF
-
-# generating another .gitignore file:
-	touch $PROJECT_DIR/$BUILD_DIR/.gitignore
-	cat > $PROJECT_DIR/$BUILD_DIR/.gitignore << EOF
-# The gitignore file specifies intentionally untracked files that Git should ignore.
-#
-# ignore everything in this directory
+#### .gitignore file generating: ####
+cat > .gitignore << EOF
+# ignore everything:
 *
-# except this file:
+
+# except:
 !.gitignore
+!Makefile
+!*.c
+!*.h
+!*/
+EOF
+# ---------------------------------- #
+
+cd $SRC_DIR
+
+#### Makefile generating: ####
+cat > Makefile << EOF
+#
+#
+
+CC	= gcc
+CFLAGS	= -Wall -ggdb -ansi -O0 -MMD -MP -fdiagnostics-color=never -fno-diagnostics-show-caret
+LIBS	= -lncurses
+MAIN_BIN= $PROJ_MAIN_BIN
+
+MODULES	= main.o
+DEPS	= \$(MODULES:.o=.d)
+
+all: \$(MAIN_BIN) tags
+
+%.o:	%.c
+	@\$(CC) \$(CFLAGS) -c $< -o \$@
+
+\$(MAIN_BIN): \$(MODULES)
+	@\$(CC) \$(CFLAGS) \$(MODULES) \$(LIBS) -o \$@
+
+.PHONY: run clean
+
+run:	\$(MAIN_BIN)
+	@./\$(MAIN_BIN)
+
+tags:
+	@ctags -R -n --c-kinds=+p --fields=+iaS --extras=+q
+
+valg:
+	valgrind -s --tool=memcheck --leak-check=full --track-origins=yes --log-file=VALG_LOG ./\$(MAIN_BIN)
+
+clean:
+	@rm -f *.o *.i *.d *.s \$(MAIN_BIN)
+
+# dependency files:
+-include \$(DEPS)
+EOF
+# ---------------------------------- #
+
+cat > main.c << EOF
+int main(void)
+{
+	return 0;
+}
 EOF
